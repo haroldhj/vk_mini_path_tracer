b6156ebd3d340456449e71ccc72fd2d5897b21f3
haroldhj <78577935+haroldhj@users.noreply.github.com>
2025-09-28 23:25:56 +1000

    compute shader

--- _edit/main.cpp
+++ _edit/main.cpp
@@ -7,9 +7,14 @@
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
+#include <nvvk/shaders_vk.hpp>
 
-static const uint64_t render_width  = 800;
-static const uint64_t render_height = 600;
+#include <nvh/fileoperations.hpp>  // For nvh::loadFile
+
+static const uint64_t render_width     = 800;
+static const uint64_t render_height    = 600;
+static const uint32_t workgroup_width  = 16;
+static const uint32_t workgroup_height = 8;
 
 int main(int argc, const char** argv)
 {
@@ -24,6 +29,20 @@ int main(int argc, const char** argv)
   VkPhysicalDeviceRayQueryFeaturesKHR rayQueryFeatures{VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_RAY_QUERY_FEATURES_KHR};
   deviceInfo.addDeviceExtension(VK_KHR_RAY_QUERY_EXTENSION_NAME, false, &rayQueryFeatures);
 
+  deviceInfo.addDeviceExtension(VK_KHR_SHADER_NON_SEMANTIC_INFO_EXTENSION_NAME);
+  VkValidationFeatureEnableEXT validationFeatureToEnable = VK_VALIDATION_FEATURE_ENABLE_DEBUG_PRINTF_EXT;
+  VkValidationFeaturesEXT      validationInfo{.sType = VK_STRUCTURE_TYPE_VALIDATION_FEATURES_EXT,
+                                              .enabledValidationFeatureCount = 1,
+                                              .pEnabledValidationFeatures    = &validationFeatureToEnable};
+  deviceInfo.instanceCreateInfoExt = &validationInfo;
+
+#ifdef _WIN32
+  _putenv_s("DEBUG_PRINTF_TO_STDOUT", "1");
+#else   // If not _WIN32
+  static char putenvString[] = "DEBUG_PRINTF_TO_STDOUT=1";
+  putenv(putenvString);
+#endif  // _WIN32
+
   context.init(deviceInfo);  // Initialize the context
 
   nvvk::ResourceAllocatorDedicated allocator;
@@ -37,11 +56,40 @@ int main(int argc, const char** argv)
   nvvk::Buffer buffer = allocator.createBuffer(bufferCreateInfo, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_CACHED_BIT
                                                                      | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
 
+  const std::string        exePath(argv[0], std::string(argv[0]).find_last_of("/\\") + 1);
+  std::vector<std::string> searchPaths = {exePath + PROJECT_RELDIRECTORY, exePath + PROJECT_RELDIRECTORY "..",
+                                          exePath + PROJECT_RELDIRECTORY "../..", exePath + PROJECT_NAME};
+
   // Command Pool
   VkCommandPoolCreateInfo cmdPoolInfo{.sType            = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,  //
                                       .queueFamilyIndex = context.m_queueGCT};
   VkCommandPool           cmdPool;
   NVVK_CHECK(vkCreateCommandPool(context, &cmdPoolInfo, nullptr, &cmdPool));
+
+  VkShaderModule rayTraceModule =
+      nvvk::createShaderModule(context, nvh::loadFile("shaders/raytrace.comp.glsl.spv", true, searchPaths));
+
+  VkPipelineShaderStageCreateInfo shaderStageCreateInfo{.sType  = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+                                                        .stage  = VK_SHADER_STAGE_COMPUTE_BIT,
+                                                        .module = rayTraceModule,
+                                                        .pName  = "main"};
+
+  VkPipelineLayoutCreateInfo pipelineLayoutCreateInfo{
+      .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO, .setLayoutCount = 0, .pushConstantRangeCount = 0};
+
+  VkPipelineLayout pipelineLayout;
+  NVVK_CHECK(vkCreatePipelineLayout(context, &pipelineLayoutCreateInfo, VK_NULL_HANDLE, &pipelineLayout));
+
+  VkComputePipelineCreateInfo pipelineCreateInfo{.sType  = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,  //
+                                                 .stage  = shaderStageCreateInfo,                           //
+                                                 .layout = pipelineLayout};
+
+  VkPipeline computePipeline;
+  NVVK_CHECK(vkCreateComputePipelines(context,                 // Device
+                                      VK_NULL_HANDLE,          // Pipeline cache (uses default)
+                                      1, &pipelineCreateInfo,  // Compute pipeline create info
+                                      VK_NULL_HANDLE,          // Allocator (uses default)
+                                      &computePipeline));      // Output
 
   // Command Buffer
   constexpr uint32_t          cmdBufCnt = 1;
@@ -56,24 +104,26 @@ int main(int argc, const char** argv)
                                      .flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT};
   NVVK_CHECK(vkBeginCommandBuffer(cmdBuffer, &beginInfo));
 
-  const float     fillValue    = 0.5f;
-  const uint32_t& fillValueU32 = reinterpret_cast<const uint32_t&>(fillValue);
-  vkCmdFillBuffer(cmdBuffer, buffer.buffer, 0, bufferSizeBytes, fillValueU32);
+
+  vkCmdBindPipeline(cmdBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, computePipeline);
+
+  vkCmdDispatch(cmdBuffer, 1, 1, 1);
+
 
   VkMemoryBarrier memoryBarrier{
       .sType         = VK_STRUCTURE_TYPE_MEMORY_BARRIER,
-      .srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT,
+      .srcAccessMask = VK_ACCESS_SHADER_WRITE_BIT,
       .dstAccessMask = VK_ACCESS_HOST_READ_BIT,
   };
-  vkCmdPipelineBarrier(cmdBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_HOST_BIT, 0, 1, &memoryBarrier, 0,
-                       nullptr, 0, nullptr);
+  vkCmdPipelineBarrier(cmdBuffer, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_PIPELINE_STAGE_HOST_BIT, 0, 1,
+                       &memoryBarrier, 0, nullptr, 0, nullptr);
 
   NVVK_CHECK(vkEndCommandBuffer(cmdBuffer));
 
   VkSubmitInfo submitInfo{
-      .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+      .sType              = VK_STRUCTURE_TYPE_SUBMIT_INFO,
       .commandBufferCount = 1,
-      .pCommandBuffers = &cmdBuffer,
+      .pCommandBuffers    = &cmdBuffer,
   };
 
   vkQueueSubmit(context.m_queueGCT, 1, &submitInfo, VK_NULL_HANDLE);
@@ -85,6 +135,11 @@ int main(int argc, const char** argv)
   allocator.unmap(buffer);
 
   vkFreeCommandBuffers(context, cmdPool, cmdBufCnt, &cmdBuffer);
+
+  vkDestroyPipeline(context, computePipeline, nullptr);
+  vkDestroyShaderModule(context, rayTraceModule, nullptr);
+  vkDestroyPipelineLayout(context, pipelineLayout, nullptr);
+
   vkDestroyCommandPool(context, cmdPool, nullptr);
 
   allocator.destroy(buffer);
